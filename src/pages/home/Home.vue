a1be10f7cf231d3f85141e9519dfe9ab46429d03
haitunyh <1132258097@qq.com>
2020-03-01 12:34:31 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,24 +1,24 @@
 <template>
-<div>
-<home-header></home-header>
-<swiper-header></swiper-header>
-
-</div>
-   
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+  </div>
 </template>
 
 <script>
-import HomeHeader from './components/Header'
-import SwiperHeader from './components/Swiper'
+import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import HomeIcons from "./components/Icons";
 export default {
-    name: 'Home',
-    components: {
-        HomeHeader: HomeHeader,
-        SwiperHeader: SwiperHeader
-    }
-}
+  name: "Home",
+  components: {
+    HomeHeader: HomeHeader,
+    HomeSwiper: HomeSwiper,
+    HomeIcons: HomeIcons
+  }
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeIcons",
+  data: function() {
+    return {
+      iconList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票1111111111111111111111"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票2"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票3"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票4"
+        },
+        {
+          id: "005",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票5"
+        },
+        {
+          id: "006",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票6"
+        },
+        {
+          id: "007",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票7"
+        },
+        {
+          id: "008",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票8"
+        },
+        {
+          id: "009",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票9"
+        }
+      ]
+    };
+  },
+  computed: {
+    pages: function() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons >>>.swiper-container {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  width: 25%;
+  height: 0;
+  float: left;
+  padding-bottom: 25%;
+}
+
+.icon-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0.44rem;
+  box-sizing: border-box;
+  padding: 0.1rem;
+}
+
+.icon-img-content {
+  display: block;
+  margin: 0 auto;
+  height: 100%;
+  align-items: center;
+}
+
+.icon-desc {
+  ellipsis();
+  position: absolute;
+  line-height: 0.44rem;
+  height: 0.44rem;
+  color: $darkTextColor;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  text-align: center;
+}
+</style>
